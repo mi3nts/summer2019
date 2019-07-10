fdfdd18ea91c31a8ccebbe5acb0063c0b3162aca
inbarmada <inbar.mada@gmail.com>
2019-07-10 10:06:19 -0500

    Update serialReader

--- inbar/serialReader.py
+++ inbar/serialReader.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 def fillDict(num, dict, data):
-  curDat = [data[num].split(":")[0].split(">")[1]] +  data[num].split(":")[1:]
+  curDat = [0] + [data[num].split(":")[0].split(">")[1]] +  data[num].split(":")[1:]
   for key, dat in zip(dict.keys(), curDat):
       dict[key] = dat
   dict["time"] = datetime.now()
@@ -29,13 +29,13 @@ contents = contents.split("#mintsO!B") #Split the file into a list of strings fo
 del contents[0] # Get rid of empty first string
 
 #Create a dictionary for each sensor's variables
-BME280 = {"var1": 0, "var2": 0, "var3": 0, "var4": 0, "time": 0}
-MGS001 = {"var1": 0, "var2": 0, "var3": 0, "var4": 0, "var5": 0, "var6": 0, "var7": 0, "var8": 0, "time": 0}
-OPCN2 = {"var1": 0, "var2": 0, "var3": 0, "var4": 0, "var5": 0, "var6": 0, "var7": 0, "var8": 0, "var9": 0, "time": 0,
+BME280 = {"time": 0, "var1": 0, "var2": 0, "var3": 0, "var4": 0}
+MGS001 = {"time": 0, "var1": 0, "var2": 0, "var3": 0, "var4": 0, "var5": 0, "var6": 0, "var7": 0, "var8": 0}
+OPCN2 = {"time": 0, "var1": 0, "var2": 0, "var3": 0, "var4": 0, "var5": 0, "var6": 0, "var7": 0, "var8": 0, "var9": 0,
          "var10": 0, "var11": 0, "var12": 0, "var13": 0, "var14": 0, "var15": 0, "var16": 0, "var17": 0,
          "var18": 0, "var19": 0, "var20": 0, "var21": 0, "var22": 0, "var23": 0, "var24": 0, "var25": 0,
-         "var26": 0, "var27": 0, "var28": 0, "time": 0}
-SCD30 = {"var1": 0, "var2": 0, "var3": 0, "time": 0}
+         "var26": 0, "var27": 0, "var28": 0}
+SCD30 = {"time": 0, "var1": 0, "var2": 0, "var3": 0}
 
 for data in contents:
     #Split vars of into sensors
@@ -44,11 +44,11 @@ for data in contents:
     #Put variables of each sensor into the list
     fillDict(0, BME280, data)
     fillDict(1, MGS001, data)
-    fillDict(2, OPCN2, data)
-    fillDict(3, SCD30, data)
+    fillDict(2, SCD30, data)
+    fillDict(3, OPCN2, data)
 
     #Make the csv file for each sensor
     makeCSV(BME280, "BME280.csv")
     makeCSV(MGS001, "MGS001.csv")
-    makeCSV(OPCN2, "OPCN2.csv")
     makeCSV(SCD30, "SCD30.csv")
+    makeCSV(OPCN2, "OPCN2.csv")
