5e999c1360ed15a81982d0ebab0e44157d1e8fa0
inbarmada <inbar.mada@gmail.com>
2019-07-10 11:43:52 -0500

    Update serialReader.py

--- inbar/serialReader.py
+++ inbar/serialReader.py
@@ -3,11 +3,11 @@ import csv
 import os
 from datetime import datetime
 
-def fillDict(num, dict, data):
-  curDat = [0] + [data[num].split(":")[0].split(">")[1]] +  data[num].split(":")[1:]
+def fillDict(dict, data):
+  curDat = [0] +  data.split(":")
   for key, dat in zip(dict.keys(), curDat):
       dict[key] = dat
-  dict["time"] = datetime.now()
+  dict["utc-time"] = datetime.utcnow()
 
 def makeCSV(dict, fileName):
     makeHeader = not(os.path.isfile(fileName))
@@ -19,36 +19,52 @@ def makeCSV(dict, fileName):
         writer.writerow(dict)
     csvFile.close()
 
+def processData(data):
+    print(data)
+    data = data.split(">")
+    if(data[0] == "#mintsO!BME280"):
+        fillDict(BME280, data[1])
+        makeCSV(BME280, "BME280.csv")
+    if(data[0] == "#mintsO!MGS001"):
+        fillDict(MGS001, data[1])
+        makeCSV(MGS001, "MGS001.csv")
+    if(data[0] == "#mintsO!SCD30"):
+        fillDict(SCD30, data[1])
+        makeCSV(SCD30, "SCD30.csv")
+    if(data[0] == "#mintsO!OPCN2"):
+        fillDict(OPCN2, data[1])
+        makeCSV(OPCN2, "OPCN2.csv")
 
-#Get data
-file = open(r"Data.txt", "r") #Read file
-contents = file.read()
-#Process data
-contents.replace("~", "") #Get rid of the ~ in the string
-contents = contents.split("#mintsO!B") #Split the file into a list of strings for each iteration of data
-del contents[0] # Get rid of empty first string
+
+
 
 #Create a dictionary for each sensor's variables
-BME280 = {"time": 0, "var1": 0, "var2": 0, "var3": 0, "var4": 0}
-MGS001 = {"time": 0, "var1": 0, "var2": 0, "var3": 0, "var4": 0, "var5": 0, "var6": 0, "var7": 0, "var8": 0}
-OPCN2 = {"time": 0, "var1": 0, "var2": 0, "var3": 0, "var4": 0, "var5": 0, "var6": 0, "var7": 0, "var8": 0, "var9": 0,
-         "var10": 0, "var11": 0, "var12": 0, "var13": 0, "var14": 0, "var15": 0, "var16": 0, "var17": 0,
-         "var18": 0, "var19": 0, "var20": 0, "var21": 0, "var22": 0, "var23": 0, "var24": 0, "var25": 0,
-         "var26": 0, "var27": 0, "var28": 0}
-SCD30 = {"time": 0, "var1": 0, "var2": 0, "var3": 0}
+BME280 = {"utc-time": 0, "temperature": 0, "pressure": 0, "humidity": 0, "altitude": 0}
+MGS001 = {"utc-time": 0, "nh3": 0, "co": 0, "no2": 0, "c3h8": 0, "c4h10": 0, "ch4": 0, "h2": 0, "c2h5oh": 0}
+OPCN2 = {"utc-time": 0, "valid": 0, "binCount0": 0, "binCount1": 0, "binCount2": 0, "binCount3": 0, "binCount4": 0, "binCount5": 0, "binCount6": 0, "binCount7": 0,
+         "binCount8": 0, "binCount9": 0, "binCount10": 0, "binCount11": 0, "binCount12": 0, "binCount13": 0, "binCount14": 0, "binCount15": 0,
+         "bin1TimeToCross": 0, "bin3TimeToCross": 0, "bin5TimeToCross": 0, "bin7TimeToCross": 0, "sampleFlowRate": 0, "temperatureOrPressure": 0, "samplingPeriod": 0, "checkSum": 0,
+         "pm1": 0, "pm2_5": 0, "pm10": 0}
+SCD30 = {"utc-time": 0, "co2": 0, "temperature": 0, "humidity": 0}
 
-for data in contents:
-    #Split vars of into sensors
-    data = data.split("~#mintsO!")
 
-    #Put variables of each sensor into the list
-    fillDict(0, BME280, data)
-    fillDict(1, MGS001, data)
-    fillDict(2, SCD30, data)
-    fillDict(3, OPCN2, data)
+#Set up serial port
+ser = serial.Serial("/dev/cu.usbserial-A90837L7", 9600, timeout=5)
 
-    #Make the csv file for each sensor
-    makeCSV(BME280, "BME280.csv")
-    makeCSV(MGS001, "MGS001.csv")
-    makeCSV(SCD30, "SCD30.csv")
-    makeCSV(OPCN2, "OPCN2.csv")
+#Read Serial setup
+i = 0
+while i < 182:
+    print(i, ser.readline())
+    i = i + 1
+
+print("SET UP COMPLETE\nREADING DATA")
+
+#Read data
+curData = ""
+while True:
+    char = (ser.read(1)).decode('utf-8')
+    if(char == '~'):
+        processData(curData)
+        curData = ""
+    else:
+        curData = curData + str(char)
